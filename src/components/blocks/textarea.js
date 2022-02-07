eceef9d09763d96a1145163c87e86624451c43ac
Lilli Marlett <lillimjimenez@gmail.com>
2022-02-07 01:20:54 -0500

    textarea

--- src/components/blocks/textarea.js
+++ src/components/blocks/textarea.js
@@ -1,19 +1,10 @@
 import React from 'react'
 
 const TextArea = ({ block }) => (
-  <section
-    className={
-      block.background
-        ? `hero is-medium has-text-centered is-${block.background}`
-        : 'hero is-medium has-text-centered'
-    }
-  >
-    <div className="hero-body">
+  <section>
       <div className="max-w-5xl">
-        <h1 className="title is-1">{block.title}</h1>
-        {block.subtitle && <p className="subtitle is-4">{block.subtitle}</p>}
+        {block.text_area}
       </div>
-    </div>
   </section>
 )
 
