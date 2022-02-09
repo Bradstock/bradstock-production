4f8ac515fc4c0a49aa3a523ada5afc9f989feee1
Lilli Marlett <lillimjimenez@gmail.com>
2022-02-09 03:35:25 -0500

    background hero

--- src/components/blocks/hero.js
+++ src/components/blocks/hero.js
@@ -3,13 +3,7 @@ import React from 'react'
 const Hero = ({ block }) => (
   console.log(block),
   <section
-    className="relative z-0 -mt-[7.75rem] min-h-72
-    `${
-      block.background
-        ? `hero is-medium has-text-centered is-${block.background}`
-        : 'hero is-medium has-text-centered'
-    }`
-    "
+    className="relative z-0 -mt-[7.75rem] min-h-72"
   >
 <div className="relative">
       <div className="absolute inset-0">
@@ -20,7 +14,7 @@ const Hero = ({ block }) => (
                 srcSet={block.background_image.childImageSharp.fluid.srcSet}
               />
         )}
-        <div className="absolute inset-0  mix-blend-multiply" aria-hidden="true" />
+        <div className="absolute inset-0 bg-white mix-blend-multiply" aria-hidden="true" />
       </div>
       <div className="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
         <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">{block.title}</h1>
