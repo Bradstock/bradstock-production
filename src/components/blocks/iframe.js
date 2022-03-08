6f1ebb815b17a596cf41c08a26465bde30e4177a
Lilli Marlett <lillimjimenez@gmail.com>
2022-03-07 20:34:32 -0500

    merge

--- src/components/blocks/iframe.js
+++ src/components/blocks/iframe.js
@@ -2,17 +2,17 @@ import React from 'react'
 
 const Iframe = ({ block }) => (
   <section
-    className='flex justify-center'
+    className='flex w-[90%] mx-auto'
   >
         <iframe 
         title='iframe block'
         src={block.url}
-        width="80%" 
+        width="100%" 
         height={block.iframe_height}
         frameborder="0" 
         marginHeight="0" 
         marginWidth="0"
-        className='my-4'
+        className='my-8'
         >
             Browser not compatible. Use this <a href={block.url} target="blank">Link </a> instead.
         </iframe>
