ad24dab62f5a26e533a788fea2e2f0cbd4dccdb0
Lilli Marlett <lillimjimenez@gmail.com>
2022-03-07 23:24:13 -0500

    fix image width

--- src/components/blocks/image.js
+++ src/components/blocks/image.js
@@ -6,7 +6,12 @@ const Image = ({ block }) => (
     className={
       block.position === 'Left' ? `w-1/4 h-auto ml-[15%] mr-4 mb-4 mt-10 float-left` :
       block.position === 'Right' ? `w-1/4 h-auto mr-[15%] ml-4 mb-4 mt-10 float-right` :
-      `w-[75%] mx-auto my-4`
+      block.position === 'Center' ? 
+        block.width === '25' ? `w-[25%] mx-auto my-4` :
+        block.width === '50' ? `w-[50%] mx-auto my-4` :
+        block.width === '75' ? `w-[75%] mx-auto my-4` : 
+        `w-[50%] mx-auto my-4` : 
+        `w-[50%] mx-auto my-4`
     }
   >
     {/* {console.log(block)} */}
--- src/components/blocks/text-area.js
+++ src/components/blocks/text-area.js
@@ -5,7 +5,9 @@ import ReactMarkdown from 'react-markdown'
 const TextArea = ({ block }) => (
   <section>
       <div className="max-w-5xl mx-auto my-10">
-      <ReactMarkdown children={block.text_area} allowDangerousHtml></ReactMarkdown>
+      <ReactMarkdown children={block.text_area} 
+      components={{img:({node,...props})=><img className='w-[60%] h-auto ml-4 mr-4 mb-4 mt-10'{...props}/>}}
+      allowDangerousHtml></ReactMarkdown>
       </div>
   </section>
 )
