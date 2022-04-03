a4b7a7b974d2501f150a51574a98cccb90ee32ba
Lilli Marlett <lillimjimenez@gmail.com>
2022-04-03 18:59:22 -0400

    make hero shorter

--- src/components/blocks/hero.js
+++ src/components/blocks/hero.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Hero = ({ block }) => (
   <section
-    className="relative z-0 -mt-[7.75rem] min-h-[35rem]"
+    className="relative z-0 -mt-[7.75rem] min-h-[27rem]"
   >
 <div className="relative">
-      <div className="absolute min-h-[35rem] inset-0">
+      <div className="absolute min-h-[27rem] inset-0">
         {block.background_image && (
               <img
                 alt={block.title}
@@ -15,15 +15,18 @@ const Hero = ({ block }) => (
         )}
         <div className="absolute inset-0 bg-stone-200 mix-blend-multiply" aria-hidden="true" />
       </div>
-      <div className="w-full mx-auto py-24 relative max-w-7xl float-left sm:w-5/6 sm:py-32 lg:w-1/2 lg:ml-16 lg:px-8 xl:w-1/3">
-        <div className='w-full m-5 px-8 py-6 rounded-2xl bg-white/40 hero-shadow ' >
-          <h1 className="text-4xl font-extrabold  text-gray-800 sm:text-5xl lg:text-6xl">
+      <div className="my-auto max-h-[25rem] max-w-[100%] md:max-w-[80%] pt-20 py-[5%] lg:pl-[10%] mx-auto lg:mx-0 relative">
+        <div className='mx-8 px-8 py-0 rounded-2xl bg-white/40 hero-shadow max-w-full lg:max-w-3xl' >
+          <h1 className="text-4xl font-extrabold text-gray-800 sm:text-5xl lg:text-6xl">
             {block.title}
           </h1>
-          <p className="mt-6 text-gray-800 text-3xl font-semibold max-w-3xl">
+          {/* </div>
+        <div className='mx-8 px-8 py-0 rounded-2xl bg-white/40 hero-shadow' > */}
+          <p className="mt-6 text-gray-800 text-xl md:text-2xl lg:text-3xl font-semibold max-w-3xl">
           {block.subtitle}
           </p>
-        </div>
+          </div>
+        
       </div>
     </div>
   </section>
