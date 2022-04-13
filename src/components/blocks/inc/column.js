7d27f5674fa605f38cac16592e8538d4fa9567da
Lilli Marlett <lillimjimenez@gmail.com>
2022-04-13 03:32:05 -0400

    stuff

--- src/components/blocks/inc/column.js
+++ src/components/blocks/inc/column.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Column = ({ columnData }) => (
   <aside>
-    <h3 className="text-cyan-700 subtitle is-4">{columnData.title}</h3>
+    {console.log(columnData)}
+    <h3 className="subtitle is-4">{columnData.title}</h3>
     <div
       className="content"
       dangerouslySetInnerHTML={{ __html: columnData.content }}
