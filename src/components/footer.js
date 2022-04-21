3876d2e2249e97b2e6daa7ea029a00e1cfefe06f
Lilli Marlett <lillimjimenez@gmail.com>
2022-04-20 23:15:27 -0400

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faEnvelope,
+} from '@fortawesome/free-regular-svg-icons'
+import {
+  faFacebook,
+} from '@fortawesome/free-brands-svg-icons'
+
 
 const Footer = ({ content }) => (
-  <footer className="flex justify-center pb-20 py-5 h-20 items-center">
-    <div className="max-w-5xl mx-auto items-center text-center" dangerouslySetInnerHTML={{__html: content}} />
+  <footer className="flex flex-col mb-8 py-5 h-48">
+    <div className='mx-auto text-center'> 
+      <h5 className='text-gray-600'>{content}</h5>
+      <div>
+        <a href='https://www.facebook.com/BradstockFest/' target='_blank' rel='noreferrer' className='mx-2'>
+          <FontAwesomeIcon className='text-cyan-700 hover:text-gray-400' icon={faFacebook} size='2x' /> 
+        </a> 
+        <a href='mailto:contact@bradstock.org' target='_blank' rel='noreferrer' className='mx-2'> 
+          <FontAwesomeIcon className='text-cyan-700 hover:text-gray-400' icon={faEnvelope} size='2x' /> 
+        </a>    
+      </div> 
+      <p className='text-gray-600'>Built by <a className='text-cyan-700 hover:text-gray-600' href="https://github.com/LMarlette" target="_blank">LMarlett</a></p>
+    </div>
   </footer>
 )
 
